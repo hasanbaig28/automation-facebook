c038eb1e22af613498e8b87e1445904fed1ebba6
hasanbaig28 <93471313+hasanbaig28@users.noreply.github.com>
2021-10-31 16:05:11 +0530

    added loginfacebook file
    added loginfacebook file

---
+++ loginfacebook02.java
@@ -0,0 +1,26 @@
+package seleniumbasic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class loginfacebook02 {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","E:\\browserdriver\\chrome D\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		WebElement email = driver.findElement(By.name("email"));
+		email.sendKeys("rajesh.prabha.376@gmail.com");
+		WebElement pass = driver.findElement(By.name("pass"));
+		pass.sendKeys("1236547891");
+		WebElement loginbtn = driver.findElement(By.name("login"));
+		loginbtn.click();
+		Thread.sleep(15000);
+		driver.close();
+	
+	}
+
+}
